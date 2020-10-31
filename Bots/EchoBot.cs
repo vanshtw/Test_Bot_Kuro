3b7ddca498b598c41cf44b67b3edf5fe542b5757
Vansh Gupta <VE00YM056@globalymc.com>
2020-10-31 20:24:01 +0530

    Add project files.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var replyText = $"{turnContext.Activity.Text}";
+            if (replyText == "")
+            {
+                replyText = "Sorry! I didn't get you!";
+            }
+
+            if (replyText.StartsWith(":") && replyText.Length == 2)
+            {
+                replyText = "Wow! An Emo!! Kuro is delighted! " + replyText;
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hey, I'm Kuro! Welcome to my channel";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
